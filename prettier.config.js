1fa0ea278485dd3743df3c31919daca2af718560
MarcosSantosDev <marcosadriano034@gmail.com>
2023-03-14 09:57:23 -0300

    feat: linter tools
    - install eslint and prettier packages;
    - create file with eslint settings;
    - create eslintignore file with file settings to be ignored by eslint;
    - create file with prettier settings;
    - create default configuration file for project operation in vscode editor;

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  printWidth: 120,
+  endOfLine: "auto",
+  bracketSpacing: true,
+};
