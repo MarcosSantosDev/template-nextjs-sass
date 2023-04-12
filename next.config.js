41a7ccbc8938e48277c7d69195b469a5d5294582
MarcosSantosDev <marcosadriano034@gmail.com>
2023-04-12 15:00:16 -0300

    feat: configure style lint plugin in next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/common/sass')],
+  },
+  webpack: config => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
   },
 };
 
