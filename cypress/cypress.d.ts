6150a856730cfb664a92d44a1c248a51689fcc49
MarcosSantosDev <marcosadriano034@gmail.com>
2023-04-13 12:33:12 -0300

    feat: configure cypress environment

---
+++ cypress/cypress.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  // interface Chainable {
+  // login(email: string, password: string): Chainable<void>
+  // drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+  // dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+  // visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+  // }
+}
