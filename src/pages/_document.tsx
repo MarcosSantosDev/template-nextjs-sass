314796814c8cdf345b2a342424bdcba0955afa42
MarcosSantosDev <marcosadriano034@gmail.com>
2023-04-14 20:12:52 -0300

    fix: remove meta tags from _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
