2be28b3fe10376eb1ea0ecd2d1a3c5137d072fe6
MarcosSantosDev <marcosadriano034@gmail.com>
2023-04-14 20:12:15 -0300

    style: style project header

--- src/common/components/context/Header/Header.tsx
+++ src/common/components/context/Header/Header.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import Image from 'next/image';
 
 import styles from './Header.module.scss';
 
 function Header({ children }: React.PropsWithChildren<unknown>) {
-  return <header className={styles.navbar}>{children}</header>;
+  return (
+    <header className={styles.navbar}>
+      <Image
+        src="next.svg"
+        alt="Next.js"
+        className={styles['navbar-image']}
+        height={16}
+        width={100}
+      />
+
+      {children}
+    </header>
+  );
 }
 
 export default Header;
